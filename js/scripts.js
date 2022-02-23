bb42fa37db4f99f9a5edaf6bcf81240f298b43e9
Greg Khmara <gkhmara@gmail.com>
2022-02-22 18:32:38 -0800

    Create pigDice project
    co-authored-by: Rian Carter <r.carter86@hotmail.com>
    co-authored-by: Erika Mogollon <fany200025@hotmail.com>
    co-authored-by: Kyle Miller <millerkyle85@gmail.com>
    co-authored-by: Marcus Ferreira <marcusferreira23@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+//CURRENT CODE 02.21.22
+function Player(playerName) {
+  this.roll = 0;
+  this.tempScore = 0;
+  this.totalScore = 0;
+  this.turn = 0;
+  this.playerName = playerName;
+}
+
+let player1 = new Player();
+let player2 = new Player();
+
+//prototype method ONLY WORKS IN CONSOLE
+Player.prototype.name = function () {
+  this.playerName = prompt("Please enter your Name:");
+}
+
+//prototype method
+Player.prototype.dieRoll = function () {
+  let firstRoll = this.roll = Math.floor(Math.random() * 6 + 1);
+  this.turn += 1;
+  if(firstRoll === 1) {
+      this.tempScore = 0;
+      console.log("You lose!");
+  } else if (firstRoll > 1) {
+      this.tempScore += firstRoll;
+      console.log(firstRoll);
+  }
+}
+Player.prototype.hold = function () {
+  this.totalScore += this.tempScore;
+  this.tempScore = 0;
+  this.roll = 0;
+  this.turn = 0; 
+}
+
+// player1.name();
+// player1.dieRoll();
+// player1.hold();
+
+//UI interface
+$(document).ready(function() {
+  $("#buttonRoll").click(function(event) {
+    event.preventDefault();
+    $("#weeknd").show()
+    player1.dieRoll();
+    $("#tempScore").text("Your temporary score: " + player1.tempScore);
+    $("#dieRoll").text("Your current die roll: " + player1.roll);
+    
+  });
+  $("#buttonHold").click(function(event) {
+    player1.hold();
+    $("#totalScore").text("Your total score: " + player1.totalScore);
+  });
+  $('#refresh').click(function() {
+    location.reload();
+  });
+
+});
