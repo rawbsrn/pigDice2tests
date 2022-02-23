8d10abfa8399503de0dfb329ff2a0aa6ac7bb050
Greg Khmara <gkhmara@gmail.com>
2022-02-22 20:28:48 -0800

    Update HTML, CSS, and JavaScript
    co-authored-by: Rian Carter <r.carter86@hotmail.com>
    co-authored-by: Erika Mogollon <fany200025@hotmail.com>
    co-authored-by: Kyle Miller <millerkyle85@gmail.com>
    co-authored-by: Marcus Ferreira <marcusferreira23@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,16 @@ let player1 = new Player();
 let player2 = new Player();
 
 //prototype method ONLY WORKS IN CONSOLE
-Player.prototype.name = function () {
+Player.prototype.name1 = function () {
   const nameInput=$("input#player1").val();
-  this.playerName = nameInput;
+  // const nameInput2=$("input#player2").val();
+  player1.playerName = nameInput;
+}
+
+//prototype method ONLY WORKS IN CONSOLE
+Player.prototype.name2 = function () {
+  const nameInput2=$("input#player2").val();
+  player2.playerName = nameInput2;
 }
 
 Player.prototype.dieRoll = function () {
@@ -36,27 +43,40 @@ Player.prototype.hold = function () {
 }
 
 Player.prototype.winner = function () {
-  if (this.totalScore >= 20) {
+  if (this.totalScore >= 100) {
     alert(this.playerName + " You won! Wow!");
   }
 }
 
 //UI interface
 $(document).ready(function() {
-  $("#buttonRoll").click(function(event) {
+  $("#buttonRoll1").click(function(event) {
     event.preventDefault();
-    player1.name();
+    player1.name1();
     $(".player1").text(player1.playerName);
     $("#weeknd").show()
     player1.dieRoll();
-    $("#tempScore").text("Your temporary score: " + player1.tempScore);
-    $("#dieRoll").text("Your current die roll: " + player1.roll);
-    
+    $("#tempScore1").text("Your temporary score: " + player1.tempScore);
+    $("#dieRoll1").text("Your current die roll: " + player1.roll);
   });
-  $("#buttonHold").click(function(event) {
+  $("#buttonHold1").click(function(event) {
     player1.hold();
-    $("#totalScore").text("Your total score: " + player1.totalScore);
+    $("#totalScore1").text("Your total score: " + player1.totalScore);
     player1.winner();
+  });
+  $("#buttonRoll2").click(function(event) {
+    event.preventDefault();
+    player2.name2();
+    $(".player2").text(player2.playerName);
+    $("#migos").show()
+    player2.dieRoll();
+    $("#tempScore2").text("Your temporary score: " + player2.tempScore);
+    $("#dieRoll2").text("Your current die roll: " + player2.roll);
+  });
+  $("#buttonHold2").click(function(event) {
+    player2.hold();
+    $("#totalScore2").text("Your total score: " + player2.totalScore);
+    player2.winner();
   });
   $('#refresh').click(function() {
     location.reload();
