38f14a75b9f10e594c1df5228371d2a209463eee
Greg Khmara <gkhmara@gmail.com>
2022-02-22 19:55:05 -0800

    Update HTML, CSS, and JavaScript
    co-authored-by: Rian Carter <r.carter86@hotmail.com>
    co-authored-by: Erika Mogollon <fany200025@hotmail.com>
    co-authored-by: Kyle Miller <millerkyle85@gmail.com>
    co-authored-by: Marcus Ferreira <marcusferreira23@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,23 +16,29 @@ Player.prototype.name = function () {
   this.playerName = nameInput;
 }
 
-//prototype method
 Player.prototype.dieRoll = function () {
-  let firstRoll = this.roll = Math.floor(Math.random() * 6 + 1);
+  this.roll = Math.floor(Math.random() * 6 + 1);
   this.turn += 1;
-  if(firstRoll === 1) {
+  if(this.roll === 1) {
       this.tempScore = 0;
       console.log("You lose!");
-  } else if (firstRoll > 1) {
-      this.tempScore += firstRoll;
-      console.log(firstRoll);
+  } else if (this.roll > 1) {
+      this.tempScore += this.roll;
+      console.log(this.roll);
   }
 }
+
 Player.prototype.hold = function () {
   this.totalScore += this.tempScore;
   this.tempScore = 0;
   this.roll = 0;
   this.turn = 0; 
+}
+
+Player.prototype.winner = function () {
+  if (this.totalScore >= 20) {
+    alert(this.playerName + " You won! Wow!");
+  }
 }
 
 //UI interface
@@ -50,9 +56,9 @@ $(document).ready(function() {
   $("#buttonHold").click(function(event) {
     player1.hold();
     $("#totalScore").text("Your total score: " + player1.totalScore);
+    player1.winner();
   });
   $('#refresh').click(function() {
     location.reload();
   });
-
 });
